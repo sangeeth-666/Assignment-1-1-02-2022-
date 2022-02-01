7b8f1e1f9bb88981d276f5cffe606edb429efee1
sangeeth <ksangeethkumar666@gmail.com>
2022-02-01 18:19:31 +0530

    no changes

---
+++ ac.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ int main()
+{
+        int r;
+        float ac,cc;
+        printf("enter the r:");
+        scanf("%d",&r);
+        ac=3.14*r*r;
+        cc=2*3.14*r;
+        printf("ac:%f,cc:%f",ac,cc);
+        return 0;
+}
+
