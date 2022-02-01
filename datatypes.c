9c5ae579ee1744d66ad0de6f0716a124dd0d20aa
sangeeth <ksangeethkumar666@gmail.com>
2022-02-01 18:25:19 +0530

    "program for print all data types"

---
+++ datatypes.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ int main()
+{
+        int x;
+        unsigned y;
+        float abc;
+        char z;
+        double d;
+        signed e;
+        printf("size of int:%ld\n",sizeof(x));
+        printf("size of unsigned:%ld\n",sizeof(y));
+        printf("size of float:%ld\n",sizeof(abc));
+        printf("size of char:%ld\n",sizeof(z));
+        printf("size of double:%ld\n",sizeof(d));
+        printf("size of signed:%ld\n",sizeof(e));
+        return 0;
+}
+
