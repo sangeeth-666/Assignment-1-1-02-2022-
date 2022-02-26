7afa7147b96ea7966b31ccc69ca73fc24053acaf
sangeeth <sangeeth@gmail.com>
2022-02-26 23:38:46 +0530

    "concatennate of two string and find lenth of new string"

---
+++ lengthofnewstring.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include<string.h>
+void strcatent(char s[],char d[]);
+
+int main()
+{
+    char s[20],d[20];
+    int len;
+    printf("enter a name1:");
+    scanf("%s",s);
+    getchar();
+    printf("enter a name2:");
+    scanf("%s",d);
+    strcatent(s,d);
+    return 0;
+}
+void strcatent(char s[],char d[])
+{
+    int i,len;
+    printf("copied string is:");
+    for(i=0;s[i]!='\0';i++)
+    {
+    printf("%c",s[i]);
+    }
+    for(i=0;d[i]!='\0';i++)
+    {
+    printf("%c",d[i]);
+    }
+    len=strlen(d);
+    printf("\nlength of new string:%d",len);
+    
+    
+    return;
+}
+
